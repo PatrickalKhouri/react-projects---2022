5b0683e3a1ae59fc24fc322a79f933259f9eb534
Patrick Khouri <patrick.darya@gmail.com>
2022-04-02 12:05:10 -0300

    Part 2

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -6,6 +6,22 @@ function App() {
   const [people, setPeople] = useState(data)
   const [index, setIndex] = useState(0)
   
+  useEffect(() => {
+    const lastIndex = people.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000)
+    return ()=> clearInterval(slider)
+  }, [index])
   return <section className='section'> 
     <div className='title'>
       <h2>
@@ -19,7 +35,7 @@ function App() {
       if (personIndex === index) {
         position = 'activeSlide'
       }
-      if (personIndex === index -1 || ) {
+      if (personIndex === index -1 || (index === 0 && personIndex === people.length - 1)){
         position = 'lastSlied'
       }
       return( <article key={id} className={position}>
@@ -30,8 +46,8 @@ function App() {
         <FaQuoteRight className='icon'/>
       </article>)
       })}
-    <button className='prev'><FiChevronLeft/></button>
-    <button className='next'><FiChevronRight/></button>
+    <button className='prev' onClick={() => setIndex( index -1)}><FiChevronLeft/></button>
+    <button className='next' onClick={() => setIndex(index + 1)}><FiChevronRight/></button>
     </div>
   </section>;
 }
