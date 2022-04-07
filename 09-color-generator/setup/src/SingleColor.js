ae86a73f3ab314502ee16ef9a99778bf3f1a9625
Patrick Khouri <patrick.darya@gmail.com>
2022-04-07 20:15:06 -0300

    Added the copy to clipboard value

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -5,14 +5,28 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false)
   const bgc = rgb.join(',')
   const hex = rgbToHex(...rgb)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [alert])
+
   return <article className={`color ${index > 8 ? 'color-light' : null}`} style={{
-    backgroundColor: `rgb(${bgc})`}}>
+    backgroundColor: `rgb(${bgc})`}}
+    onClick={() => {
+      setAlert(true);
+      navigator.clipboard.writeText(hex)
+
+    }}>
     <p className='percent-value'>
       {weight}%
     </p>
     <p className='color-value'>
       {hex}
     </p>
+    {alert && <p className='alert'> Copied to clipboard</p>}
   </article>
 }
 
