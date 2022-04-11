869c9523e4685dc2d76c82e7acdde7bf5b5f9954
Patrick Khouri <patrick.darya@gmail.com>
2022-04-11 17:11:49 -0300

    Did the edit and remove buttons

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -7,25 +7,39 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false)
   const [editId, setEditIt] = useState(null)
-  const [alert, setAlert] = useState({show: false, message: '', type: ''})
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''})
 
   const handleSubmit = (e) => {
     e.preventDefault() 
     if (!name) {
-
+      showAlert(true,'danger', 'Please enter value')
     }
     else if (name && isEditing) {
-
+      showAlert(true,'Please enter value','danger')
     } else {
+      showAlert(true, 'success', 'item added to the list')
       const newItem = {id: new Date().getTime().toString(), title: name}
-      setList([...newItem])
+      setList([...list, newItem])
       setName('')
     }
   }
 
+  const showAlert = (show=false, type='', msg='') => {
+    setAlert({show, type, msg})
+  }
+
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list')
+    setList([])
+  }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item) => item.id !== id))
+  }
   return <section className='section-center'>
     <form action="grocery-form" onSubmit={handleSubmit}>
-      {alert.show && <Alert/>}
+      {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}
       <h3>grocery bud</h3>
       <div className='form-control'>
         <input type="text" className='grocery' placeholder='example: Eggs' value={name} onChange={(e) => setName(e.target.value)}/>
@@ -35,8 +49,8 @@ function App() {
       </div>
     </form>
     {list.length > 0 && (<div className='grocery-container'>
-      <List items={list}/>
-      <button className='clear-btn'>Clear Items</button>
+      <List items={list} removeItem={removeItem}/>
+      <button className='clear-btn' onClick={clearList}>Clear Items</button>
     </div>)}
 
   </section>
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items}) => {
+const List = ({items, removeItem}) => {
 
   return <div className='grocery-list'>
     {items.map((item) => {
@@ -10,7 +10,7 @@ const List = ({items}) => {
           <button type='button' className='edit-btn'>
             <FaEdit></FaEdit>
           </button>
-          <button type='button' className='delete-btn'>
+          <button type='button' className='delete-btn' onClick={() => removeItem(item.id)}>
             <FaTrash></FaTrash>
           </button>
         </div>
