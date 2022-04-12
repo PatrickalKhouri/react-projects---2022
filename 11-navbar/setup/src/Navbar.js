a0458073442b8cc8e936c818499988ed8bb0bdda
Patrick Khouri <patrick.darya@gmail.com>
2022-04-12 15:59:01 -0300

    Basic structure of the navbar and some links

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,45 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+  <nav>
+    <div className='nav-center'>
+      <div class="nav-header">
+        <img src={logo} alt="logo"/>
+        <button className='nav-toggle'>
+          <FaBars />
+        </button>
+      </div>
+      <div class="links-container show-container">
+        <ul class="links">
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">about</a>
+          </li>
+          <li>
+            <a href="#">contacts</a>
+          </li>
+          <li>
+            <a href="#">products</a>
+          </li>
+        </ul>
+      </div>
+        <ul className='social-icons'>
+        <li>
+          <a href="https://www.twitter.com"><FaTwitter/></a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com"><FaTwitter/></a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com"><FaTwitter/></a>
+        </li>
+        </ul>
+    </div>
+  </nav>
+  )
 }
 
 export default Navbar
